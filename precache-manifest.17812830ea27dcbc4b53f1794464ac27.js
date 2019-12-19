6136ae9c80879a38fce39398a800a990cc92cc2e
atrifanoff <angelo.trifanoff@gmail.com>
2019-12-18 22:28:31 -0500

    Updates

---
+++ precache-manifest.17812830ea27dcbc4b53f1794464ac27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58aabb1ff6c325d8cb4c511681a7bc62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "166c1278be134930328c",
+    "url": "/static/css/main.8acb00e6.chunk.css"
+  },
+  {
+    "revision": "2211df7e843cfe6400fe",
+    "url": "/static/js/2.77084cdf.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.77084cdf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "166c1278be134930328c",
+    "url": "/static/js/main.2c499e76.chunk.js"
+  },
+  {
+    "revision": "dbf5b037e2acb28ea4db",
+    "url": "/static/js/runtime-main.4aeccd9a.js"
+  },
+  {
+    "revision": "608425a432df7241eea462edbcf5cb5a",
+    "url": "/static/media/rvin_white_logo.608425a4.png"
+  }
+]);
